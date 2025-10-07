fa1c2f4657000682234f97b97878b0dec32d382a
milenialdev <carlosbenitoandev@gmail.com>
2025-10-07 12:41:41 +0200

    file structure for tests created

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    // Dónde buscar los archivos de test
+    include: ['tests/**/*.test.ts'],
+    
+    // Variables globales disponibles sin importar
+    globals: true,
+    
+    // Entorno de ejecución
+    environment: 'node',
+  }
+});
